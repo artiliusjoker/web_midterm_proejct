929c22c7ef6db9e3476eaed94f09dbab0412231a
Artilius <haydayclonee@gmail.com>
2019-11-03 00:41:14 +0700

    User Websites

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Homepage' });
 });
-
+router.get('/blog', function(req, res, next) {
+  res.render('pages/web/blog', { title: 'Blog' });
+});
+router.get('/regular', function(req, res, next) {
+  res.render('pages/web/regular', { title: 'Reviews' });
+});
+router.get('/shop', function(req, res, next) {
+  res.render('pages/web/shop', { title: 'Shop' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('pages/web/contact', { title: 'Contact' });
+});
+router.get('/product', function(req, res, next) {
+  res.render('pages/web/product', { title: 'Contact' });
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/:username/signin', function(req, res, next) {
+  res.render('pages/users/signin', { title: 'Sign In Successfully' });
+});
+router.get('/:username/signup', function(req, res, next) {
+  res.render('pages/users/signin', { title: 'Sign Up Successfully' });
+});
+router.get('/:username/cart', function(req, res, next) {
+  res.render('pages/users/cart', { title: 'Your cart' });
+});
+router.get('/:username/update', function(req, res, next) {
+  res.render('pages/users/update', { title: 'Update user info' });
+});
+router.get('/:username/cart/delivery', function(req, res, next) {
+  res.render('pages/users/delivery', { title: 'Delivery info' });
+});
 module.exports = router;
