1be4fdc16f64634c3e77066e695c472d600c63f3
Artilius <haydayclonee@gmail.com>
2019-11-03 22:09:44 +0700

    Hoan thanh phan user

--- routes/users.js
+++ routes/users.js
@@ -5,19 +5,22 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/:username/signin', function(req, res, next) {
-  res.render('pages/users/signin', { title: 'Sign In Successfully' });
+router.get('/reset', function(req, res, next) {
+  res.render('pages/users/forgotpass', { title: 'Reset password' });
 });
-router.get('/:username/signup', function(req, res, next) {
-  res.render('pages/users/signin', { title: 'Sign Up Successfully' });
+router.get('/signin', function(req, res, next) {
+  res.render('pages/users/signin', { title: 'Sign In' });
 });
-router.get('/:username/cart', function(req, res, next) {
+router.get('/signup', function(req, res, next) {
+  res.render('pages/users/signup', { title: 'Sign Up' });
+});
+router.get('/cart', function(req, res, next) {
   res.render('pages/users/cart', { title: 'Your cart' });
 });
-router.get('/:username/update', function(req, res, next) {
+router.get('/update', function(req, res, next) {
   res.render('pages/users/update', { title: 'Update user info' });
 });
-router.get('/:username/cart/delivery', function(req, res, next) {
+router.get('/cart/delivery', function(req, res, next) {
   res.render('pages/users/delivery', { title: 'Delivery info' });
 });
 module.exports = router;
